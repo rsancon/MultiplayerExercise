04e6b46803bd499de385a0879e247bd88585baac
Rafael Dias Sançon <rafael.sancon@kokku.local>
2024-12-04 22:52:28 -0300

    Base project

---
+++ Source/MultiplayerExercise/MultiplayerExerciseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerExerciseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerExerciseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerExerciseGameMode();
+};
+
+
+
